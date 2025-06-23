eb2b39753f51037d67b70baf437dfdec91442906
Chris Mathew Aje <chrismaje63@gmail.com>
2025-06-23 15:45:43 +0530

    tailwind configure.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        royal: '#2563EB',
-        gold: '#FACC15',
-        softGray: '#F3F4F6',
-        slate: '#1F2937',
-        textPrimary: '#111827',
-        textSecondary: '#6B7280',
+        royal: "#2563EB",
+        gold: "#FACC15",
+        softGray: "#F3F4F6",
+        slate: "#1F2937",
+        textPrimary: "#111827",
+        textSecondary: "#6B7280",
       },
     },
   },
