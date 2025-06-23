5a0c4824d22e55209f63ed4f2e43ab63293aa4db
Chris Mathew Aje <chrismaje63@gmail.com>
2025-06-23 12:43:15 +0530

    Update for theme switching also.

--- src/components/SkylineBackground.jsx
+++ src/components/SkylineBackground.jsx
@@ -1,13 +1,13 @@
 // this is temperoary.. until the skyline is actually setup, and deployed.
-import { useEffect, useState } from "react";
+import { useTheme } from "../context/ThemeContext";
 
 export default function SkylineBackground() {
-  const [darkMode, setDarkMode] = useState(false);
+  const { theme } = useTheme();
 
   return (
     <div
       className={`fixed inset-0 -z-10 transition-colors duration-500 ${
-        darkMode ? "bg-[#1F2937]" : "bg-[#F3F4F6]"
+        theme === "dark" ? "bg-slate" : "bg-softGray"
       }`}
     />
   );
