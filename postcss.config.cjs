55a253d3e6f1e347bca2a662e07c12f1017aab90
Chris Mathew Aje <chrismaje63@gmail.com>
2025-06-23 16:08:04 +0530

    Visual version fix. Styles and tailwind is loading.

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+/** @type {import('postcss').ProcessOptions} */
+module.exports = {
+  plugins: [tailwindcss, autoprefixer],
+};
