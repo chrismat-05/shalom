3a67cb28ee7588439b17cd2eb75cd805430ab7ed
Chris Mathew Aje <chrismaje63@gmail.com>
2025-06-25 15:19:17 +0530

    Fixed scroll to top on new page open.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Ministries from "./pages/Ministries";
 import Header from "./components/Header";
 import StickyFooter from "./components/StickyFooter";
 import VerseToast from "./components/VerseToast";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="relative z-10 min-h-screen">
         <Header />
         <Routes>
