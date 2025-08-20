0b9871f357b3fb8da388354368c51b8702bf3cf8
Chris Mathew Aje <chrismaje63@gmail.com>
2025-08-20 17:54:07 +0530

    Cleaned up everything. Removed everything for starting with Landing

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-/** @type {import('postcss').ProcessOptions} */
-module.exports = {
-  plugins: [tailwindcss, autoprefixer],
-};
